79c97513ae2f761d235438b42bc4895a439521d5
Ray Hinch <rahinch@icloud.com>
2021-02-13 06:34:55 -0500

    Log In Modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// PREVENT MOVING TO QUOTE PAGE WITHOUT ENTERING FREIGHT TYPE
 getQuote.addEventListener("click",toQuotePage)
-
 function toQuotePage(){
     if (document.getElementById('ftl_index').checked) {
         window.location.href="ftl_quotes.html#ftl_quote_info";
@@ -9,3 +9,12 @@ function toQuotePage(){
         alert("Please select FTL or LTL");
     }
 }
+
+// PREVENTS OVER 5 DIGIT ZIP ON INDEX PAGE QUOTE BOX 
+
+
+$(function() {
+    $("#loginButton").click(function () {
+      $("#loginModal").modal('show');
+    });
+  });
