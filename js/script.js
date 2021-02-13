31b90a0d9f99ed8f11240ddb02945d8d92abbadd
Ray Hinch <rahinch@icloud.com>
2021-02-13 05:00:20 -0500

    LTL Quote Page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function toQuotePage(){
     if (document.getElementById('ftl_index').checked) {
         window.location.href="ftl_quotes.html#ftl_quote_info";
     } else if (document.getElementById('ltl_index').checked) {
-        window.location.href="ltl_quotes.html";
+        window.location.href="ltl_quotes.html#ltl_quote_info";
     } else {
         alert("Please select FTL or LTL");
     }
